ed93c3ac66b1770dec5dec940fc090bfa3184445
praful chandra <praful.chandra@yahoo.com>
2020-04-25 17:14:41 +0530

    Updates

---
+++ precache-manifest.f088d4cc9edec6d807760f7e7850d393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990d8938054826333ecf7aa4ce68d538",
+    "url": "/monsters_inc/index.html"
+  },
+  {
+    "revision": "b13584f9434a1d27cf79",
+    "url": "/monsters_inc/static/css/main.8d0e3cad.chunk.css"
+  },
+  {
+    "revision": "c196f0f89617fdf6a7e9",
+    "url": "/monsters_inc/static/js/2.1314f64b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_inc/static/js/2.1314f64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13584f9434a1d27cf79",
+    "url": "/monsters_inc/static/js/main.a1eda57e.chunk.js"
+  },
+  {
+    "revision": "090f44c61190f90ed055",
+    "url": "/monsters_inc/static/js/runtime-main.8307fb7e.js"
+  }
+]);
